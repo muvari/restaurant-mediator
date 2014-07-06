df520c359f8e64282a3ebeb0d32c92519faaacd2
Mark Uvari <muvari@gmail.com>
2014-07-06 18:54:18 -0400

    Merge with DragDropGrid project

---
+++ restaurantmediator/src/restaurantmediator/SurveyActivity.java
@@ -0,0 +1,36 @@
+package restaurantmediator;
+
+import com.muvari.restaurantmediator.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SurveyActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.survey_fragment);
+	}
+
+	public static class SurveyFragment extends Fragment {
+
+		public SurveyFragment() {
+		}
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			return super.onCreateView(inflater, container, savedInstanceState);
+		}
+	}
+}
