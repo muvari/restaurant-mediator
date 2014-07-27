701b7e0970b0eea2d9f83016757b77b5c4d15bb3
Mark Uvari <muvari@gmail.com>
2014-07-27 13:57:15 -0400

    Update GPS code and styles

--- restaurantmediator/src/com/muvari/restaurantmediator/mediator/StartSurveyActivity.java
+++ restaurantmediator/src/com/muvari/restaurantmediator/mediator/StartSurveyActivity.java
@@ -30,12 +30,14 @@ import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnValueChangeListener;
 
 /**
- * Starting Activity. Includes basic instructions and where you state location and member of group
+ * Starting Activity. Includes basic instructions and where you state location
+ * and member of group
+ * 
  * @author Mark
- *
+ * 
  */
 public class StartSurveyActivity extends FragmentActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,30 +45,28 @@ public class StartSurveyActivity extends FragmentActivity {
 		setContentView(R.layout.scrollable_activity);
 		if (savedInstanceState == null) {
 			FragmentManager fragmentManager = getSupportFragmentManager();
-			FragmentTransaction fragmentTransaction = fragmentManager
-					.beginTransaction();
-	
+			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
 			StartSurveyFragment fragment = new StartSurveyFragment();
-			fragmentTransaction.add(R.id.scrollview, fragment,
-					StartSurveyFragment.START_FRAGMENT_TAG);
+			fragmentTransaction.add(R.id.scrollview, fragment, StartSurveyFragment.START_FRAGMENT_TAG);
 			fragmentTransaction.commit();
 		}
 	}
-	
+
 	public static class StartSurveyFragment extends Fragment {
 
 		public static final String START_FRAGMENT_TAG = "start_fragment";
 		public static final String ADDRESS_TAG = "address_tag";
 		public static final String NUM_PEOPLE_TAG = "num_people_tag";
 		public static final String CUR_PERSON_TAG = "cur_person_tag";
-		
+
 		private NumberPicker picker;
 		private ImageView people;
-		private static final int[] peopleDrawables = { R.drawable.people_1, R.drawable.people_2, R.drawable.people_3, R.drawable.people_4,
-			R.drawable.people_5, R.drawable.people_6, R.drawable.people_7, R.drawable.people_8, R.drawable.people_9};
-		
+		private static final int[] peopleDrawables = { R.drawable.people_1, R.drawable.people_2, R.drawable.people_3, R.drawable.people_4, R.drawable.people_5,
+				R.drawable.people_6, R.drawable.people_7, R.drawable.people_8, R.drawable.people_9 };
+
 		private EditText addressText;
-	
+
 		@Override
 		public void onSaveInstanceState(Bundle outState) {
 			super.onSaveInstanceState(outState);
@@ -77,28 +77,26 @@ public class StartSurveyActivity extends FragmentActivity {
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View view = inflater.inflate(R.layout.start_survey_fragment, container,
-					false);
-			
-			people = (ImageView)view.findViewById(R.id.people);
-			picker = (NumberPicker)view.findViewById(R.id.numberPicker1);
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.start_survey_fragment, container, false);
+
+			people = (ImageView) view.findViewById(R.id.people);
+			picker = (NumberPicker) view.findViewById(R.id.numberPicker1);
 			picker.setMinValue(getResources().getInteger(R.integer.min_users));
 			picker.setMaxValue(getResources().getInteger(R.integer.max_users));
 			picker.setValue(savedInstanceState != null ? savedInstanceState.getInt("people") : 2);
-			people.setImageResource(peopleDrawables[picker.getValue()-1]);
+			people.setImageResource(peopleDrawables[picker.getValue() - 1]);
 			picker.setOnValueChangedListener(new OnValueChangeListener() {
 
 				@Override
 				public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-					people.setImageResource(peopleDrawables[newVal-1]);
+					people.setImageResource(peopleDrawables[newVal - 1]);
 				}
-				
+
 			});
-			
-			addressText = (EditText)view.findViewById(R.id.editText1);
-			
+
+			addressText = (EditText) view.findViewById(R.id.editText1);
+
 			addressText.setOnTouchListener(new OnTouchListener() {
 
 				@Override
@@ -108,9 +106,9 @@ public class StartSurveyActivity extends FragmentActivity {
 				}
 
 			});
-			
-			//Starts the Surveys
-			Button start = (Button)view.findViewById(R.id.start_button);
+
+			// Starts the Surveys
+			Button start = (Button) view.findViewById(R.id.start_button);
 			start.setOnClickListener(new OnClickListener() {
 
 				@Override
@@ -118,44 +116,69 @@ public class StartSurveyActivity extends FragmentActivity {
 					if (addressText.getText().toString().length() < 5) {
 						addressText.setError(getResources().getString(R.string.address_error));
 					} else {
-					Intent intent = new Intent(getActivity(), SurveyActivity.class);
-					intent.putExtra(ADDRESS_TAG, addressText.getText().toString());
-					intent.putExtra(NUM_PEOPLE_TAG, picker.getValue());
-					intent.putExtra(CUR_PERSON_TAG, 1);
-					startActivity(intent);
+						Intent intent = new Intent(getActivity(), SurveyActivity.class);
+						intent.putExtra(ADDRESS_TAG, addressText.getText().toString());
+						intent.putExtra(NUM_PEOPLE_TAG, picker.getValue());
+						intent.putExtra(CUR_PERSON_TAG, 1);
+						startActivity(intent);
 					}
 				}
-				
+
 			});
-			
-			//Use GPS to detect a location
-			Button detect = (Button)view.findViewById(R.id.detect_location);
+
+			// Use GPS to detect a location
+			Button detect = (Button) view.findViewById(R.id.detect_location);
 			detect.setOnClickListener(new OnClickListener() {
+				
+				/**
+				 * try to get the 'best' location selected from all providers
+				 */
+				private Location getBestLocation() {
+					Location gpslocation = getLocationByProvider(LocationManager.GPS_PROVIDER);
+					if (gpslocation != null)
+						return gpslocation;
+					
+					Location networkLocation = getLocationByProvider(LocationManager.NETWORK_PROVIDER);
+					if (networkLocation != null)
+						return networkLocation;
+					
+					return null;
+				}
+
+				/**
+				 * get the last known location from a specific provider (network/gps)
+				 */
+				private Location getLocationByProvider(String provider) {
+					Location location = null;
+					LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+					location = locationManager.getLastKnownLocation(provider);
+					return location;
+				}
 
 				@Override
 				public void onClick(View v) {
-					LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-					String locationProvider = LocationManager.NETWORK_PROVIDER;
-
-					Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
-					
-					Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
-					List<Address> addresses;
-					try {
-						addresses = geocoder.getFromLocation(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude(), 1);
-						addressText.setText(addresses.get(0).getAddressLine(0) + " " + addresses.get(0).getAddressLine(1));
-					} catch (IOException e) {
-						e.printStackTrace();
+					Location lastKnownLocation = getBestLocation();
+					if (lastKnownLocation != null) {
+						Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+						List<Address> addresses;
+						try {
+							addresses = geocoder.getFromLocation(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude(), 1);
+							addressText.setText(addresses.get(0).getAddressLine(0) + " " + addresses.get(0).getAddressLine(1));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					} else {
+						addressText.setError(getResources().getString(R.string.address_error2));
 					}
-					
+
 				}
-				
+
 			});
-			
-			
+
 			return view;
 		}
+
 	}
 
 }
